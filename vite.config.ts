1a01187be2cf4c560c12c16b7f2b49f6119b9d19
JIHOON LEE <jihoon.lee@hicare.net>
2025-08-13 17:03:00 +0900

    feat: api 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://www.satto.io.kr',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
   },
   build: {
     target: 'es2015',
