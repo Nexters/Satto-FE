5c329cb54bb1dfc59e1405c3a1f1dd08eca4765a
JIHOON LEE <jihoon.lee@hicare.net>
2025-08-14 10:41:25 +0900

    chore: capacitor setting

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.satto.app',
+  appName: 'satto-history-app',
+  webDir: 'dist'
+};
+
+export default config;
